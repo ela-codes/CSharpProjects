335833a897961cc31330fa2d172e7673ba0476c0
ela-codes <elacodes@gmail.com>
2025-07-30 20:07:09 -0500

    feat(calculator): implement basic cli calculator interface

---
+++ CalculatorInCSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿// Create a CLI calculator
+
+// Ask user what math operator to use
+// Accepts 2 integers
+// Outputs the result
+
+string userOperator = "";
+
+bool isValidOperator = false;
+
+string[] validOperators = { "add", "subtract", "divide", "multiply" };
+
+while (!isValidOperator)
+{
+    Console.Write("Choose an operation (add / subtract / divide / multiply): ");
+    userOperator = Console.ReadLine();
+    isValidOperator = validOperators.Contains(userOperator);
+}
+
+if (isValidOperator)
+{
+    Console.Write("Enter the first integer: ");
+    int firstNum = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter the second integer: ");
+    int secondNum = int.Parse(Console.ReadLine());
+
+    int result = 0;
+
+    switch (userOperator)
+    {
+        case "add":
+            result = firstNum + secondNum;
+            break;
+
+        case "subtract":
+            result = firstNum - secondNum;
+            break;
+
+        case "divide":
+            result = firstNum / secondNum;
+            break;
+
+        case "multiply":
+            result = firstNum * secondNum;
+            break;
+    }
+
+    Console.WriteLine($"The result is: {result}");
+}
